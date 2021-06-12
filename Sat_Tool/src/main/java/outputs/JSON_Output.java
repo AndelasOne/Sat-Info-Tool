e3f0926f372e6bb9e4c2804debb0e1c540144d05
Andreas Weber <Andreas.S.Weber@outlook.com>
2021-06-12 17:48:30 +0200

    added outputs

---
+++ Sat_Tool/src/main/java/outputs/JSON_Output.java
@@ -0,0 +1,9 @@
+package outputs;
+
+import dhbw.swe.IOutput;
+
+public class JSON_Output implements IOutput {
+    public String output() {
+        return "Hello";
+    }
+}
--- Sat_Tool/src/main/java/plugins/ProgramCounter.java
+++ Sat_Tool/src/main/java/plugins/ProgramCounter.java
@@ -3,5 +3,5 @@ package plugins;
 import dhbw.swe.IPlugin;
 
 public class ProgramCounter implements IPlugin {
-    
+
 }
